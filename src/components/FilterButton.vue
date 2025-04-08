cf1751793ab28bda461944e17b48a8da2ff5f7cb
weiwei <wutamy77@gmail.com>
2025-04-08 10:32:08 +0800

    create filter button component

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { TaskFilter } from '../types';
+
+const props = defineProps<{
+  filter: TaskFilter;
+  currentFilter: TaskFilter;
+}>();
+
+const emits = defineEmits<{
+  setFilter: [filter: TaskFilter]
+}>();
+</script>
+
+<template>
+  <button 
+    class="secondary"
+    @click="emits('setFilter', props.filter)"
+    :class="{
+      contrast: props.currentFilter === props.filter
+    }"
+  >
+    {{ props.filter }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  text-transform: capitalize;
+}
+</style>
