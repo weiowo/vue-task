76f8ed815fa6a3a052c7379d7077a9ba0c6a72d8
weiwei <wutamy77@gmail.com>
2025-04-08 10:24:23 +0800

    add taskform component

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+  addTask: [newTask: string]
+}>();
+
+const newTask = ref("");
+const error = ref("");
+
+function formSubmitted() {
+  if (newTask.value.trim()) {
+    emit("addTask", newTask.value.trim());
+    newTask.value = "";
+  } else {
+    error.value = "Task cannot be empty!";
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="formSubmitted">
+    <label>
+      New Task
+      <input
+        v-model="newTask"
+        name="newTask"
+        :aria-invalid="!!error || undefined"
+        @input="error = ''"
+      >
+      <small v-if="error" id="invalid-helper">
+        {{ error }}
+      </small>
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
